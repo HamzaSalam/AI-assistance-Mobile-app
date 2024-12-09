54243acbe53a550e2f033824055587a3203ac823
hamza salam <hamza.salam786@gmail.com>
2024-12-09 12:33:18 +0500

    remove chat api

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,4 @@
-export const apiKey =
-  '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';
+export const apiKey = '';
 
 export const dummyMessages = [
   {role: 'user', content: 'How are you'},
